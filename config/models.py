4dec5702ff2e3d47ace30037c75ed228f67af5d5
Kai Zhang <kzhang@microfocus.com>
2019-07-05 10:10:59 +0800

    the first merge

--- config/models.py
+++ config/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.loader import render_to_string
 # Create your models here.
 
 
@@ -24,18 +25,23 @@ class Link(models.Model):
 
 
 class SideBar(models.Model):
+    DISPLAY_HTML = 1
+    DISPLAY_LATEST = 2
+    DISPLAY_HOT = 3
+    DISPLAY_COMMENT = 4
+    SIDE_TYPE = (
+        {DISPLAY_HTML, 'HTML'},
+        {DISPLAY_LATEST, 'Latest Posts'},
+        {DISPLAY_HOT, 'Hottest Posts'},
+        {DISPLAY_COMMENT, 'Latest Comments'},
+    )
     STATUS_SHOW = 1
     STATUS_HIDE = 0
     STATUS_ITEMS = (
         {STATUS_SHOW, 'show'},
         {STATUS_HIDE, 'hide'},
     )
-    SIDE_TYPE = (
-        {1, 'HTML'},
-        {2, 'Latest Posts'},
-        {3, 'Hottest Posts'},
-        {4, 'Latest Comments'},
-    )
+
     title = models.CharField(max_length=50, verbose_name='title')
     display_type = models.PositiveIntegerField(default=1, choices=SIDE_TYPE, verbose_name='display type')
     content = models.CharField(max_length=500, blank=True, verbose_name='post')
@@ -45,3 +51,34 @@ class SideBar(models.Model):
 
     class Meta:
         verbose_name = verbose_name_plural = 'side bar'
+
+    @classmethod
+    def get_all(cls):
+        return cls.objects.filter(status=cls.STATUS_SHOW)
+
+    @property
+    def content_html(self):
+        """直接渲染模板"""
+        from blog.models import Post
+        from comment.models import Comment
+
+
+        result = ''
+        if self.display_type == self.DISPLAY_HTML:
+            result = self.content
+        elif self.display_type == self.DISPLAY_LATEST:
+            context = {
+                'posts': Post.latest_posts()
+            }
+            result = render_to_string('config/blocks/sidebar_posts.html', context)
+        elif self.display_type == self.DISPLAY_HOT:
+            context = {
+                'posts': Post.hot_posts()
+            }
+            result = render_to_string('config/blocks/sidebar_posts.html', context)
+        elif self.display_type == self.DISPLAY_COMMENT:
+            context = {
+                'comments': Comment.objects.filter(status=Comment.STATUS_NORMAL)
+            }
+            result = render_to_string('config/blocks/sidebar_comments.html', context)
+        return result
--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+sys.path.append("venv/lib/python3.6/site-packages/")
 
 
 def main():
